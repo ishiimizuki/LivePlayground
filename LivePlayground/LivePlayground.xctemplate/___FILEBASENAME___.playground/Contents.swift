2174aac4ff7e5c6b8214897b489798d95c9d310e
ishiimizuki <ishiimizuki1192@gmail.com>
2016-04-21 23:51:31 +0900

    add file

---
+++ LivePlayground/LivePlayground.xctemplate/___FILEBASENAME___.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import XCPlayground
+
+XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+
+class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.whiteColor()
+    }
+}
+
+let vc = ___FILEBASENAMEASIDENTIFIER___()
+XCPlaygroundPage.currentPage.liveView = vc
